f6a125a2641bb78c9c50e91977d29fd237afafb7
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2025-03-12 23:22:21 +0530

    Addes search compo & new package

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, TextInput } from "react-native";
+import React, { useState } from "react";
+import { useLocalSearchParams, usePathname } from "expo-router";
+import icons from "@/constants/icons";
 
 const Search = () => {
+  const path = usePathname();
+  const params = useLocalSearchParams<{ query?: string }>();
+  const [search, setSearch] = useState(params.query || "");
+  const handleSearch = (text: string) => {
+    setSearch(text);
+  };
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex flex-row items-center justify-between mt-5 w-full px-4 rounded-lg bg-accent-100 border border-primary-100 py-2">
+      <View className="flex flex-row items-center flex-1 z-50 justify-start">
+        <Image source={icons.search} className="size-5" />
+        <TextInput />
+      </View>
     </View>
   );
 };
